0ebdba173802677077f10b95f080b245d024f922
Erik Eppel <eceppel@gmail.com>
2015-08-18 19:59:19 -0700

    [Progress] adds directions factory definition

---
+++ www/js/services/directions.js
@@ -0,0 +1,18 @@
+/* 
+* For information on the Directions Service, see the docs ( https://developers.google.com/maps/documentation/javascript/directions )
+ */
+
+angular.module('parkAssist')
+  .factory('Directions', function() {
+    var directions = {
+      // returns an instance of the DirectionsService object 
+      directionsObj: function() {
+        return new google.maps.DirectionsService();
+      },
+      // returns an instance of the DirectionsRenderer object
+      directionsRenderer: function() {
+        return new google.maps.DirectionsRenderer();
+      }
+    };
+    return directions;
+  })
