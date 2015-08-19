5bbad138b0d00237e6af3f7a0252fbe801968c18
Erik Eppel <eceppel@gmail.com>
2015-08-19 14:43:01 -0700

    Cleans up directive syntax

--- www/js/directives/map-directive.js
+++ www/js/directives/map-directive.js
@@ -1,10 +1,10 @@
 angular.module('parkAssist')
   .directive('map', function(MapFactory) {
-    var mapFactory = MapFactory;
+
     var loadMap = function(scope, element, attrs) {
       var $el = $(element);
       var mapCanvas = $el.find('#map')[0];
-      mapFactory.init(mapCanvas);
+      MapFactory.init(mapCanvas);
     };
 
     return {
