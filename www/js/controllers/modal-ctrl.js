3b42b1f7ab7bef4891ff22eb5053f46f95311550
Erik Eppel <eceppel@gmail.com>
2015-08-21 15:08:24 -0700

    [Feature] adds modal search

--- www/js/controllers/modal-ctrl.js
+++ www/js/controllers/modal-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('modalAssist', ['parkAssist'])
-  .controller('ModalCtrl', function($scope, $ionicModal, $state, $cordovaGeolocation, MapOptions, TrafficLayer, $rootScope, MapFactory) {
+  .controller('ModalCtrl', function($scope, $ionicModal, $state, $cordovaGeolocation, MapOptions, TrafficLayer, $rootScope, MapFactory, $ionicPopup, $timeout) {
 	 
 	 $scope.searched = {
 	   text: ''
@@ -16,6 +16,16 @@ angular.module('modalAssist', ['parkAssist'])
 	    $scope.modal.show();
 	  };
 
+	  $scope.showAlert = function() {
+	    var alertPopup = $ionicPopup.alert({
+	      title: 'That\'s outside of Santa Monica',
+	      template: 'Please select a Santa Monica location'
+	    });
+	    alertPopup.then(function(res) {
+	      $scope.openModal();
+	    });
+	  };
+
 	  $scope.submitDestination = function() {
 	  	
 	  	var text = $scope.searched.text;
@@ -26,7 +36,8 @@ angular.module('modalAssist', ['parkAssist'])
 	      MapFactory.findSpot([text.geometry.location.G, text.geometry.location.K], true);
 	    } else {
 	    	console.log( 'You selected something outside of Santa Monica' );
-	      $rootScope.$broadcast('$scope.searched.text', 'Please select a Santa Monica Location.');
+	      // $rootScope.$broadcast('$scope.searched.text', 'Please select a Santa Monica Location.');
+	      $scope.showAlert();
 	    }
 	    $scope.modal.hide();
 	  };
